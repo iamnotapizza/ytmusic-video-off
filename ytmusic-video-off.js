cb25569a8f76817df66a729bda0b889cb1a3e503
iamnotapizza <knight.sharar@gmail.com>
2022-01-12 01:51:00 -0600

    player is deleted instead of hiding and extended view has playlist centered horizontally

--- ytmusic-video-off.js
+++ ytmusic-video-off.js
@@ -1,1 +1,19 @@
- document.getElementById('player').style.visibility = "hidden";
+//not necessary anymore as we delete the entire main panel
+/* const player = document.getElementById('player');
+player.style.visibility = "hidden";*/
+
+
+//delete the player
+ const mainpanel = document.getElementById('main-panel');
+ mainpanel.remove();
+
+//horizontally centers the sidepanel box 
+ const sidepanel = document.getElementsByClassName('side-panel');
+ sidepanel[1].style.margin = "0px auto 0px auto";
+ //playlist[1].style.margin = "auto";
+
+
+ //seperate up next, lyrics and related into taking 3 parts of the screen
+ // further see if you can add albumart or if video a thumbnail
+
+
